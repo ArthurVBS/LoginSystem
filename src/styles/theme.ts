98abaa4cc0c030f46c5025e03b6fe1e112283f55
ArthurVBS <84406367+ArthurVBS@users.noreply.github.com>
2022-07-10 18:38:29 +0000

    Deploying to gh-pages from @ ArthurVBS/LoginSystem@9af9e6430a0420f1ac26b3ee3e1691105adb7526 🚀

--- src/styles/theme.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  primary: '#7689de',
-  secondary: '#a786b8',
-  background: '#FAFAED',
-  text: '#171717',
-}
--- src/utils/env_sample.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_URL =
-  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8090'
