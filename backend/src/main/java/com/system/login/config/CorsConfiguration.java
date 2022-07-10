7c182bd8fc3be1f9bb3ab61095f15a270908d7f8
ArthurVBS <arthurvini2703@gmail.com>
2022-07-10 16:44:09 -0300

    Update CORS config: allow access by github pages

--- backend/src/main/java/com/system/login/config/CorsConfiguration.java
+++ backend/src/main/java/com/system/login/config/CorsConfiguration.java
@@ -2,15 +2,17 @@ package com.system.login.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins( "https://arthurvbs.github.io", "http://localhost:3000")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
   }
 }
